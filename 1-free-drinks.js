645202113c4aaaf749ef499056586a83fb252687
atmabdullaham <atmabdullaham@gmail.com>
2024-11-07 11:04:38 +0600

    make solutions of last 3 maths of type1 problems

--- 1-free-drinks.js
+++ 1-free-drinks.js
@@ -55,10 +55,10 @@ function getDiscount(purchasedItems) {
 
 }
 
-console.log(getDiscount(12));
-console.log(getDiscount(11));
-console.log(getDiscount(9));
-console.log(getDiscount(10));
+// console.log(getDiscount(12));
+// console.log(getDiscount(11));
+// console.log(getDiscount(9));
+// console.log(getDiscount(10));
 
 /**
  * 3. Shipping Fee Calculator
@@ -67,6 +67,17 @@ console.log(getDiscount(10));
  *    - Write a function that takes the total order amount and returns the final price including any shipping fees.
  */
 
+function getShippingFee(orderAmount) {
+    return orderAmount > 800 ? "Shipping is free" : "The shipping fee is 40 Taka"
+}
+// console.log(getShippingFee(500));
+// console.log(getShippingFee(800));
+// console.log(getShippingFee(900));
+
+
+
+
+
 /**
  * 4. Cinema Ticket Pricing
  *    - A cinema offers a 50% discount on tickets for people under the age of 18 or over the age of 60.
@@ -74,9 +85,29 @@ console.log(getDiscount(10));
  *    - Write a function that accepts the age of a person and returns the ticket price based on the applicable discount, if any.
  */
 
+function getTicketPrice(age) {
+    return age < 18 || age > 60 ? "You got 50% discount on ticket price" : "The ticket Price is 200tk"
+}
+
+// console.log(getTicketPrice(17));
+// console.log(getTicketPrice(18));
+// console.log(getTicketPrice(19));
+// console.log(getTicketPrice(59));
+// console.log(getTicketPrice(60));
+// console.log(getTicketPrice(61));
+
 /**
  * 5. Loyalty Card Points
  *    - A store offers 2 points per dollar spent for purchases of 500 Taka or more.
  *    - For purchases less than 500 Taka, the customer earns 1 point per dollar spent.
  *    - Write a function that takes the purchase amount and returns the total loyalty points earned.
  */
+function getLoyaltyPoints(purchaseAmount) {
+    return purchaseAmount >= 500 ? "Eligible to get 2 points for per dollar spent" : "Eligible to get 1 points for per dollar spent"
+}
+
+console.log(getLoyaltyPoints(500));
+console.log(getLoyaltyPoints(400));
+console.log(getLoyaltyPoints(600));
+console.log(getLoyaltyPoints(501));
+console.log(getLoyaltyPoints(499));
